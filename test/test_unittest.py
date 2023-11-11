d11928d3741a1740d5ce30d8025389b35d616f4a
Adit Bhosle <aditbhosle81@gmail.com>
2023-11-10 22:40:05 -0500

    Troubleshoot Unittest

--- test/test_unittest.py
+++ test/test_unittest.py
@@ -30,8 +30,34 @@ class TestPipeline(unittest.TestCase):
         
         df = pickle.loads(xcom_result)
 
-        #self.assertTrue(len(df) > 0, "DataFrame should not be empty")
+        self.assertTrue(len(df) > 0, "DataFrame should not be empty")
         self.assertEqual(df.shape[1], 68, "Number of columns should be 68" )
+
+    def test_ohe(self):
+
+        dag_id = 'pipeline'
+        dag = self.dagbag.get_dag(dag_id)
+
+        dag.clear()
+        dag.run()
+
+        task_instance = dag.get_task('ohe_task')
+        xcom_result = task_instance.xcom_pull(task_ids='ohe_task')
+
+        df = pickle.loads(xcom_result)
+
+        def is_one_hot_encoded(column):
+            unique_values = column.unique()
+            if set(unique_values) == {0, 1}:
+                if 1 in unique_values:
+                    if column.sum() == len(column):
+                        return True
+                    
+        male = is_one_hot_encoded(df['gender_M'])
+        female = is_one_hot_encoded(df['gender_F'])
+
+        self.assertEqual(male, True)
+        self.assertEqual(female, True)
 
 if __name__ == 'main':
     unittest.main()
