f7c7792b40053442e8d9e577f95a6c8e99861730
Adit Bhosle <aditbhosle81@gmail.com>
2023-11-10 20:56:07 -0500

    Troubleshoot unittest

--- dags/airflow.py
+++ dags/airflow.py
@@ -2,7 +2,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-from .src.data import load_data, data_preprocessing, drop, convert_strdate_to_datetime, slicer, merge_category, drop_2, ohe
+from src.data import load_data, data_preprocessing, drop, convert_strdate_to_datetime, slicer, merge_category, drop_2, ohe
 from airflow import configuration as conf
 
 conf.set('core', 'enable_xcom_pickling', 'True')
