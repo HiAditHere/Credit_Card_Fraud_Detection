d863983fcbe184baa3a8ee1532083730159aa984
Adit Bhosle <aditbhosle81@gmail.com>
2023-11-12 15:35:28 -0500

    Troubleshoot Workflow

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -1,6 +1,7 @@
 # Import necessary libraries and modules
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 from dags.source.data_preprocess import load_data, data_preprocessing, drop, convert_strdate_to_datetime, slicer, merge_category, drop_2, ohe
 from airflow import configuration as conf
@@ -24,7 +25,7 @@ dag = DAG(
     schedule_interval=None, # Set the schedule interval or use None for manual triggering
     catchup=False,
 )
-
+'''
 load_data_task = PythonOperator(
     task_id='load_data_task',
     python_callable=load_data,
@@ -32,12 +33,7 @@ load_data_task = PythonOperator(
     dag=dag,
 )
 
-'''data_preprocessing_task = PythonOperator(
-    task_id='data_preprocessing_task',
-    python_callable=data_preprocessing,
-    op_args=[load_data_task.output],
-    dag=dag,
-)'''
+
 
 drop_task = PythonOperator(
     task_id='drop_task',
@@ -82,7 +78,24 @@ ohe_task = PythonOperator(
 )
 
 #load_data_task >> data_preprocessing_task
-load_data_task >> drop_task >> convert_strdate_to_datetime_task >> slicer_task >> merge_category_task >> drop_2_task >> ohe_task
+load_data_task >> drop_task >> convert_strdate_to_datetime_task >> slicer_task >> merge_category_task >> drop_2_task >> ohe_task'''
+
+'''data_preprocessing_task = PythonOperator(
+    task_id='data_preprocessing_task',
+    python_callable=data_preprocessing,
+    op_args=[load_data_task.output],
+    dag=dag,
+)'''
+
+dag = DAG('pipeline', default_args=default_args, schedule_interval='@daily')
+
+start_task = DummyOperator(task_id='start_task', dag=dag)
+task_1 = DummyOperator(task_id='task_1', dag=dag)
+task_2 = DummyOperator(task_id='task_2', dag=dag)
+
+end_task = DummyOperator(task_id='end_task', dag=dag)
+
+start_task >> task_1 >> task_2 >> end_task
 
 # If this script is run directly, allow command-line interaction with the DAG
 if __name__ == "__main__":
