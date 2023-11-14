9cc3cb7ad1c75a55c891915feae8f18183315a85
Adit Bhosle <aditbhosle81@gmail.com>
2023-11-14 00:18:59 -0500

    Troublershoot workflow

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -2,7 +2,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-from dags.source.data_preprocess import load_data, data_preprocessing, drop, convert_strdate_to_datetime, slicer, merge_category, drop_2, ohe
+from source.data_preprocess import load_data, data_preprocessing, drop, convert_strdate_to_datetime, slicer, merge_category, drop_2, ohe
 from airflow import configuration as conf
 import os
 
