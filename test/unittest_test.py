0c96bc09d980ea6a31483859568319dff4ea68d1
Adit Bhosle <aditbhosle81@gmail.com>
2023-12-07 05:59:27 -0500

    Troubleshoot workflow fail for merge

--- test/unittest_test.py
+++ test/unittest_test.py
@@ -2,6 +2,7 @@ import sys
 import os
 import unittest
 from airflow.models import DagBag
+from airflow.operators.base_operator import BaseOperator
 import pandas as pd
 
 # Get the path to the project's root directory
@@ -23,6 +24,13 @@ class TestPipeline(unittest.TestCase):
         """Check the task dependencies of drop_task in pipeline dag"""
         dag_id='pipeline2'
         dag = self.dagbag.get_dag(dag_id)
+
+        for task_id, task in dag.task_dict.items():
+            if isinstance(task, BaseOperator):
+                print(f"Task ID: {task_id}, Task Type: {type(task).__name__}")
+            else:
+                print(f"Task ID: {task_id}, Not an instance of BaseOperator")
+
         drop_task = dag.get_task('drop_task')
 
 
